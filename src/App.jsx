73ef7ea323c2e5082b8635eca856e5bc5732214c
KARTIK ORAON <114291823+7kartik090@users.noreply.github.com>
2024-07-28 13:12:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 import Footer from "./components/Footer";
 function App() {
   return (
@@ -10,4 +8,3 @@ function App() {
 }
 
 export default App;
->>>>>>> f2c6e1af358a6114bec9c87fe7d458c1016eab38
