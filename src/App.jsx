2215bfc51173bd0d4b269d42ad895957f5e6a8ef
Shailesh Kumar Soren <shaileshsoren62@gmail.com>
2024-07-28 14:15:42 +0530

    blog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import Blog from "./components/BlogComponents/Blog";
+import BlogDetail from "./components/BlogComponents/BlogDetail";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/blog" element={<Blog />} />
+        <Route path="/" element={<Blog />} />
         <Route path="/blog/:id" element={<BlogDetail />} />
       </Routes>
     </Router>
+    // <>
+    // <Blog />
+    // </>
+    
+    
   );
 };
 
