8fcc04e0d38c591cc62fec88278042b81993759f
SONU KUMAR SINGH <sonukrsingh0987@gmail.com>
2024-07-29 17:18:58 +0530

    updated contact page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Layout from './components/Layout';
 import Shop from './components/shop/SustainableClothing';
 import ProductDetail from './components/shop/ProductDetail';
 import About from './components/Home/About';
+import Contact from './components/Home/Contact';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/shop" element={<Shop />} />
         <Route path="/product/:productId" element={<ProductDetail />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
 
     </Routes>
