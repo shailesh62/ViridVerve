d3e73314163c109cf9dc67121452117d9950f865
7kartik090 <koraon287@gmail.com>
2024-08-03 00:13:52 +0530

    added carousel

---
+++ src/components/HomePageComponents/CarouselData.jsx
@@ -0,0 +1,33 @@
+// carouselData.js
+const carouselData = [
+  {
+    id: "slide1",
+    url: "https://images-ext-1.discordapp.net/external/EGqOVGVcXuRFU2x9B_fCr-U9-qQljdpa-1ijvrh__AY/%3Fq%3D80%26w%3D1974%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D/https/images.unsplash.com/photo-1558769132-cb1aea458c5e?format=webp&width=846&height=635",
+    alt: "Eco-Friendly Fashion",
+    slogan: "Eco-Friendly Fashion",
+    description: "Explore our range of eco-friendly clothing crafted from sustainable materials. Make a positive impact with every purchase."
+  },
+  {
+    id: "slide2",
+    url: "https://images-ext-1.discordapp.net/external/d1BKP7nAem_XRaPQGoX7tdxI0lOJ5Wfy7Y6XihzyasI/%3Fauto%3Dcompress%26cs%3Dtinysrgb%26w%3D600/https/images.pexels.com/photos/6626999/pexels-photo-6626999.jpeg?format=webp&width=750&height=500",
+    alt: "Sustainable Style",
+    slogan: "Sustainable Style",
+    description: "Embrace a stylish and eco-conscious lifestyle with our sustainable clothing collection. Fashion that cares for the planet."
+  },
+  {
+    id: "slide3",
+    url: "https://images-ext-1.discordapp.net/external/dKaiqdRJAgRfdo-VLFCBm5QfgjQ8SHKsieKX0VQ_h6M/%3Fq%3D80%26w%3D2070%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D/https/images.unsplash.com/photo-1441986300917-64674bd600d8?format=webp&width=951&height=635",
+    alt: "Green and Chic",
+    slogan: "Green and Chic",
+    description: "Discover clothing that combines chic design with environmental responsibility. Look good and do good with our green collection."
+  },
+  {
+    id: "slide4",
+    url: "https://images-ext-1.discordapp.net/external/OfusAb7uDlKY6RmP-SEYkUwrxGlWL1a5cfJkxPWHp7Y/%3Fq%3D80%26w%3D2070%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D/https/images.unsplash.com/photo-1483985988355-763728e1935b?format=webp&width=952&height=635",
+    alt: "Eco-Conscious Choices",
+    slogan: "Eco-Conscious Choices",
+    description: "Make eco-conscious choices with our range of sustainable clothing. Every piece is designed with both style and the planet in mind."
+  }
+];
+
+export default carouselData;
